13db3579335095f56e694c4f0e4e8d40b5b47027
Jorge <jorgebravo202131782@cunoc.edu.gt>
2024-08-18 17:08:36 -0600

    inicio graficos

--- Graficador/src/main/java/com/jbrod/graficador/Graficador.java
+++ Graficador/src/main/java/com/jbrod/graficador/Graficador.java
@@ -1,6 +1,13 @@
 
 package com.jbrod.graficador;
 
+import com.jbrod.graficador.analizadores.Lexer;
+import com.jbrod.graficador.analizadores.Parser;
+import java.io.StringReader;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+
 /**
  *
  * @author jorge
@@ -8,6 +15,20 @@ package com.jbrod.graficador;
 public class Graficador {
 
     public static void main(String[] args) {
-        System.out.println("olaolaolaaaa");
+
+        try {
+            System.out.println("olaolaolaaaa");
+            
+            String s = "graficar poligono ( PoligA, 12 + 2, 15, 6, 50 / 2*(12-8/2), 12, amarillo)"
+                    +  "graficar circulo (figura_1, 25 + 12, 25, 15/3, rojo)"
+                    +  "graficar linea (linea_Amarillo, 12 * 3 + 2, 15, 4 / 4, 50 * 1, amarillo)"
+                    +  "graficar rectangulo (rectangulo_12, 12 * 3 + 2, 15, 4 / 4, negro)";
+            StringReader reader = new StringReader(s);
+            Lexer lexer = new Lexer(reader);
+            Parser parser = new Parser(lexer);
+            parser.parse();
+        } catch (Exception ex) {
+            Logger.getLogger(Graficador.class.getName()).log(Level.SEVERE, null, ex);
+        }
     }
 }
